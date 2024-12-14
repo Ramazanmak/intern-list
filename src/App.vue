3f93081a36129c8fe4d88674922dbb5393d38ef6
Ramazan Maksyutov <ramazanmaksyutov@outlook.com>
2024-12-14 22:04:20 +0300

    I've made MyHeader, included it into the App and made files for MyList and ToolPanel

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import counter from './components/counter.vue'
+  import {ref} from 'vue'
+  import MyHeader from "./components/MyHeader.vue"
+  import MyList from "./components/MyList.vue"
+  import ToolPanel from './components/ToolPanel.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-  <counter/>
+  <MyHeader></MyHeader>
+  <main>
+
+  </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  main{
+    display:flex;
+    flex-direction:row;
+    justify-content:space-between;
+  }
 </style>

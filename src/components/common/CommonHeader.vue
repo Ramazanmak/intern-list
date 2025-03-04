b72f213624de519322b844738363d1b537ce83e6
Ramazan Maksyutov <ramazanmaksyutov@outlook.com>
2025-03-04 16:43:35 +0300

    logo changed

--- src/components/common/CommonHeader.vue
+++ src/components/common/CommonHeader.vue
@@ -4,7 +4,7 @@
 <template>
     <header>
         <h1> Список <span style="color:orange">стажёров</span></h1>
-        <img src="../../assets/imgs/loodsen-logo-modern.svg">
+        <img src="../../assets/imgs/images.png">
     </header>
 </template>
 
@@ -23,10 +23,10 @@
         font-size:5vh;
     }
     img{
-        width:15%;
+        width:10%;
         display:block;
         position:absolute;
         right:20px;
-        top:10px;
+        top:5px;
     }
 </style>
